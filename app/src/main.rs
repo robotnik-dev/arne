bc08dec1d2fc5ac7dc41d555e9c84336c1535b5f
Robotnik <robotnik.games@gmail.com>
2024-04-20 11:55:33 +0200

    switch to bevy

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,15 +1,13 @@
-use bevy_ecs::prelude::*;
-
-mod app;
-
-fn process() {
-    todo!()
-}
+use bevy::prelude::*;
+use genetic_algorithm::GeneticAlgorithmPlugin;
 
 fn main() {
-    let mut app = app::App::new();
-
-    app.schedule.add_systems(process);
-
-    app.run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(GeneticAlgorithmPlugin)
+        .run();
 }
+
+// idea: using bevys states to change from initiaizing to selection to crossover to mutation
+// because selection needs parralelism and should evaluate all agents at ones as fast as possible
+// And use system sets to run all system at ones when in this particular state
--- crates/genetic_algorithm/src/lib.rs
+++ crates/genetic_algorithm/src/lib.rs
@@ -1,16 +1,31 @@
+// this library is a generic implementation of a genetic algorithm
+// it uses the entity component system pattern to represent the agents and to leverage 
+// the parallelism for evaluating the fitness of each agent
 
-pub struct GeneticAlgorithm;
+use bevy::prelude::*;
 
-impl GeneticAlgorithm {
-    pub fn evolve<T>(&self, population: &Vec<T>) -> Vec<T> {
-        population
-            .iter()
-            .map(|_| {
-                // selection
-                // crossover
-                // mutation
-                todo!()
-            })
-            .collect()
+pub struct GeneticAlgorithmPlugin;
+
+impl Plugin for GeneticAlgorithmPlugin {
+    fn build(&self, app: &mut App) {
+        // app.init_resource::<MyOtherResource>();
+        // app.add_event::<MyEvent>();
+        app.add_systems(Startup, initialize_population);
+        // app.add_systems(Update, my_system);
     }
+}
+
+#[derive(Bundle)]
+struct AgentBundle {
+    // marker
+    agent: Agent
+}
+
+#[derive(Component)]
+struct Agent;
+
+/// At the beginning, a set of solutions,
+/// which is denoted as population, is initialized.
+fn initialize_population() {
+    todo!()
 }
