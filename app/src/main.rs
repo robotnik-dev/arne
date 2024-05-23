eb48d29973bf0d8c9cbf6b0e2e41d4064c6745bf
Robotnik <robotnik.games@gmail.com>
2024-05-23 18:50:52 +0200

    visualization update

--- app/src/main.rs
+++ app/src/main.rs
@@ -10,9 +10,9 @@ type Error = Box<dyn std::error::Error>;
 type Result = std::result::Result<(), Error>;
 
 const POPULATION_SIZE: usize = 100;
-const MAX_GENERATIONS: u32 = 600;
+const MAX_GENERATIONS: u32 = 1;
 const NEURON_PER_RNN: usize = 5;
-const NUMBER_OF_RNN_UPDATES: usize = 10;
+const NUMBER_OF_RNN_UPDATES: usize = 20;
 const GREYSCALE_TO_MATCH: SimpleGrayscale = SimpleGrayscale(255);
 
 trait Genotype {}
@@ -36,11 +36,11 @@ struct SimpleGrayscale(u8);
 
 impl Phenotype for SimpleGrayscale {}
 
-//TODO: Rename later
-#[derive(Debug, Clone)]
-struct RealPhenotype(f32);
+// //TODO: Rename later
+// #[derive(Debug, Clone)]
+// struct RealPhenotype(f32);
 
-impl Phenotype for RealPhenotype {}
+// impl Phenotype for RealPhenotype {}
 
 struct Population<G: Genotype, P: Phenotype> {
     agents: Vec<Agent<G,P>>,
@@ -97,8 +97,15 @@ impl AgentEvaluation<SimpleGrayscale> for Agent<RNN, SimpleGrayscale> {
 
     fn evaluate(&mut self, data: SimpleGrayscale, number_of_updates: usize) -> f64 {
         let mut local_fitness = 0.0;
-        for _ in 0..number_of_updates {
+        self.genotype.short_term_memory.clear();
+        for i in 0..number_of_updates {
             self.genotype.update();
+            // creating snapshot of the network at the current time step
+            let snapshot = SnapShot {
+                outputs: self.genotype.neurons.iter().map(|neuron| neuron.output).collect(),
+                time_step: (i + 1) as u32,
+            };
+            self.genotype.short_term_memory.add_snapshot(snapshot);
             // CLONING here is okay because its only a u8, but for other implementations it might be a problem
             local_fitness += self.calculate_fitness(data.clone());
         }
@@ -106,15 +113,15 @@ impl AgentEvaluation<SimpleGrayscale> for Agent<RNN, SimpleGrayscale> {
     }
 }
 
-impl Clone for Agent<RNN, RealPhenotype> {
-    fn clone(&self) -> Self {
-        Agent {
-            fitness: 0.,
-            genotype: self.genotype.clone(),
-            phenotype: self.phenotype.clone(),
-        }
-    }
-}
+// impl Clone for Agent<RNN, RealPhenotype> {
+//     fn clone(&self) -> Self {
+//         Agent {
+//             fitness: 0.,
+//             genotype: self.genotype.clone(),
+//             phenotype: self.phenotype.clone(),
+//         }
+//     }
+// }
 
 impl Agent<RNN, SimpleGrayscale> {
     fn new(number_of_neurons: usize) -> Self {
@@ -126,9 +133,117 @@ impl Agent<RNN, SimpleGrayscale> {
     }
 }
 
+/// A short term memory that can be used to store the state of the network
+#[derive(Clone)]
+struct ShortTermMemory {
+    snapshots: Vec<SnapShot>,
+}
+
+impl ShortTermMemory {
+    fn new() -> Self {
+        ShortTermMemory {
+            snapshots: vec![],
+        }
+    }
+
+    fn clear(&mut self) {
+        self.snapshots.clear();
+    }
+
+    fn get_neuron_count(&self) -> usize {
+        self.snapshots.first().map(|snapshot| snapshot.outputs.len()).unwrap_or(0)
+    }
+
+    fn get_timesteps(&self) -> Vec<u32> {
+        self.snapshots.iter().map(|snapshot| snapshot.time_step).collect()
+    }
+
+    fn add_snapshot(&mut self, snapshot: SnapShot) {
+        self.snapshots.push(snapshot);
+    }
+
+    fn get_snapshot_at_timestep(&self, time_step: u32) -> Option<&SnapShot> {
+        self.snapshots.iter().find(|snapshot| snapshot.time_step == time_step)
+    }
+
+    /// function that returns a list of tuples of (timestep, neuron_output) as a vector
+    /// for evry snapshot that was already saved so that it can be visualized
+    /// E.g. if we saved two snapshots at timestep 0 and 1, the function will return (asuuming we have 5 neurons)
+    /// [
+    ///     [
+    ///         (1, get_snapshot_at_timestep(1).outputs[0]),
+    ///         (2, get_snapshot_at_timestep(2).outputs[0])
+    ///     ],
+    ///     [
+    ///         (1, get_snapshot_at_timestep(1).outputs[1]),
+    ///         (2, get_snapshot_at_timestep(2).outputs[1])
+    ///     ],
+    ///    [...]
+    /// ]
+    fn get_visualization_data(&self) -> Option<Vec<Vec<(u32, f64)>>> {
+        let mut data = vec![];
+        for idx in 0..self.get_neuron_count() {
+            let mut snapshot_data = vec![];
+            for timestep in self.get_timesteps() {
+                let snapshot = self.get_snapshot_at_timestep(timestep)?;
+                snapshot_data.push((timestep, snapshot.outputs[idx]));
+            }
+            data.push(snapshot_data);
+        }
+        Some(data)
+    }
+
+    fn visualize(&self, to_filename: String) -> Result {
+        let path = format!("images/{}.png", to_filename);
+        let root = BitMapBackend::new(&path, (1024, 768)).into_drawing_area();
+        root.fill(&WHITE)?;
+        let visualization_data = self.get_visualization_data().ok_or("IndexError: cant get visualization data")?;
+        
+        let drawing_areas = root.split_evenly((visualization_data.len(),1));
+
+        for i in 0..drawing_areas.len() {
+            let mut chart_builder = ChartBuilder::on(&drawing_areas[i]);
+
+            chart_builder
+                // .caption(format!("N{}", i), ("sans-serif", 16).into_font())
+                .margin(5)
+                .set_all_label_area_size(20);
+            
+            let mut chart_context = chart_builder
+                .build_cartesian_2d(1u32..20u32, -1.0f64..1.0f64)?;
+        
+            chart_context
+                .configure_mesh()
+                .disable_x_mesh()
+                .max_light_lines(1)
+                .y_labels(3)
+                .x_labels(1)
+                .draw()?;
+    
+            let color = Palette99::pick(i);
+            chart_context
+                .draw_series(LineSeries::new(visualization_data[i].iter().map(|(x, y)| (*x, *y)), &color))?;
+        }
+
+        root.present()?;
+
+        Ok(())
+    }
+}
+
+/// a snapshot of the network at a certain point in time.
+/// This can be used to restore the network to a previous state but its mainly used
+/// to store the outputs of the neurons to visulaize the network
+#[derive(Clone)]
+struct SnapShot {
+    outputs: Vec<f64>,
+    time_step: u32,
+}
+
 #[derive(Clone)]
 struct RNN {
     neurons: Vec<Neuron>,
+    short_term_memory: ShortTermMemory,
 }
 
 impl Genotype for RNN {}
@@ -179,13 +294,14 @@ impl RNN {
             .for_each(|(index, neuron)| {
                 for i in 0..neuron_count {
                     if i != index {
-                        neuron.input_connections.push((i, thread_rng().gen_range(-10.0..=10.0)));
+                        neuron.input_connections.push((i, thread_rng().gen_range(-1.0..=1.0)));
                     }
                 }
             });
         
         RNN {
             neurons,
+            short_term_memory: ShortTermMemory::new(),
         }
     }
 
@@ -237,9 +353,9 @@ impl Neuron {
             index,
             output: 0.,
             input_connections: vec![],
-            bias: thread_rng().gen_range(-10.0..=10.0),
+            bias: thread_rng().gen_range(-1.0..=1.0),
             // randomize self activation between -1 and 1
-            self_activation: thread_rng().gen_range(-10.0..=10.0),
+            self_activation: thread_rng().gen_range(-1.0..=1.0),
         }
     }
 }
@@ -280,9 +396,12 @@ fn main() -> Result {
     }
     
     println!("Stopped at generation {}", population.generation);
-    println!("Best Fitness: {}", population.agents[0].fitness);
+    
+    // visualize the best agent
+    let best_agent = population.agents.first().unwrap();
+    best_agent.genotype.short_term_memory.visualize("best_agent".into())?;
 
-Ok(())
+    Ok(())
 }
 
 #[cfg(test)]
@@ -458,6 +577,51 @@ mod tests {
         let fitness = agent.evaluate(SimpleGrayscale(127), 2);
         let fitness2 = agent2.evaluate(SimpleGrayscale(127), 2);
         
+
+    }
+
+    #[test]
+    fn test_create_snapshots() {
+        // create a new rnn with 3 neurons
+        // update the rnn 5 times
+        // after each update create a snapshot
+        // check if the snapshots are correct
+        
+        let mut rnn = RNN::new(3);
+        rnn.neurons[0].output = 0.97;
+        rnn.neurons[1].output = 0.88;
+        rnn.neurons[2].output = 0.39;
+        
+        for i in 0..1 {
+            let snapshot = SnapShot {
+                outputs: rnn.neurons.iter().map(|neuron| neuron.output).collect(),
+                time_step: i,
+            };
+            rnn.short_term_memory.add_snapshot(snapshot);
+            let saved_snapshot = rnn.short_term_memory.get_snapshot_at_timestep(i).unwrap();
+            assert_eq!(saved_snapshot.outputs[0], 0.97);
+            assert_eq!(saved_snapshot.outputs[1], 0.88);
+            assert_eq!(saved_snapshot.outputs[2], 0.39);
+        }
+    }
+    
+    #[test]
+    fn test_visualize() {
+        // create a new rnn with 3 neurons
+        // update the rnn 5 times
+        // after each update create a snapshot
+        // visualize the rnn
+        let mut rnn = RNN::new(3);
+
+        for i in 1..=5 {
+            rnn.update();
+            let snapshot = SnapShot {
+                outputs: rnn.neurons.iter().map(|neuron| neuron.output).collect(),
+                time_step: i,
+            };
+            rnn.short_term_memory.add_snapshot(snapshot);
+        }
+        rnn.short_term_memory.visualize("test_output".into()).unwrap();
 
     }
 
